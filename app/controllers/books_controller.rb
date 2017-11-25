44f494be0e2b4d03bfbd11dd2381f655698f3127
nafi durmus <nafidurmus07@gmail.com>
2017-11-25 14:14:05 +0300

    Created show.html.erb && added new books for db

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
+	before_action :find_book, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@books = Book.all.order("created_at DESC")
+	end
+
+	def show
 	end
 
 	def new 
@@ -9,6 +14,14 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(book_params)
+
+		if @book.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+		
+
 	end
 
 	
@@ -16,5 +29,9 @@ class BooksController < ApplicationController
 
 	def book_params
 		params.require(:book).permit(:title, :description, :author)
+	end
+
+	def find_book
+		@book = Book.find(params[:id])
 	end
 end
