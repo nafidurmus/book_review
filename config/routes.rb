71d8cf8143141883858e7a03c1b534b95a911764
nafi durmus <nafidurmus07@gmail.com>
2017-12-02 12:18:08 +0300

    Created reviews views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   	devise_for :users
-	resources :books #"books" controller name
+	resources :books do #"books" controller name
+		resources :reviews
+	end
 
 	root 'books#index' #ilk açılınca gelecek saayfanın yolu
 end
