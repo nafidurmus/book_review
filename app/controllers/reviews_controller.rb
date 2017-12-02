9d03771700abce7b27ec4d76ff0e0a29d9f88373
nafi durmus <nafidurmus07@gmail.com>
2017-12-02 13:56:27 +0300

    like a finish Build a Book Review App - Part 4 & ı will fix it

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,52 +1,57 @@
 class ReviewsController < ApplicationController
-	before_action :find_book
-	before_action :find_review, only: [:edit, :update, :destroy]
-	#before_action :authenticate_user!, only: [:new, :edit]
+  before_action :find_book
+  before_action :find_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+  # before_action :has_reviewed, only: [:new]
 
-	def new
-		@review = Review.new
-	end
+  def new
+    @review = Review.new
+  end
 
-	def create
-		@review = Review.new(review_params)
-		@review.book_id = @book.id
-		@review.user_id = current_user.id
+  def create
+    @review = Review.new(review_params)
+    @review.book_id = @book.id
+    @review.user_id = current_user.id
 
-		if @review.save
-			redirect_to book_path(@book)
-		else
-			render 'new'
-		end
-	end
+    if @review.save
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		if @review.update(review_params)
-			redirect_to book_path(@book)
-		else
-			render 'edit'
-		end
-	end
+  def update
+    if @review.update(review_params)
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@review.destroy
-		redirect_to book_path(@book)
-	end
+  def destroy
+    @review.destroy
+    redirect_to book_path(@book)
+  end
 
-	private
+  private
 
-		def review_params
-			params.require(:review).permit(:rating, :comment)
-		end
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
 
-		def find_book
-			@book = Book.find(params[:book_id])
-		end
+    def find_book
+      @book = Book.find(params[:book_id])
+    end
 
-		def find_review
-			@review = Review.find(params[:id])
-		end
+    def find_review
+      @review = Review.find(params[:id])
+    end
+
+    # def has_reviewed
+    #   redirect_to book_path(@book), notice: "You've already written a review for this book!" if current_user.reviews.exists?(book: @book)
+    # end
 
 end
